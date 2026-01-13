0d60442e3a241af0ca841efeba94eb8534e1327e
fotsingmatakouhuguette-gif <fotsingmatakouhuguette@gmail.com>
2026-01-13 16:10:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HUGUETTE-project
---
+++ riddler.py
@@ -0,0 +1,45 @@
+import pygame
+import sys
+
+pygame.init()
+
+pygame.mixer.init()
+
+
+# ---------- CONSTANTS ----------
+WIDTH, HEIGHT = 800, 600
+GROUND_LEVEL = 430
+GRAVITY = 1
+JUMP_FORCE = -22
+MAX_LIVES = 3
+RIDDLE_TIME = 30
+FEEDBACK_DURATION = 1200
+
+# ---------- STATES ----------
+MENU = "menu"
+PLAYING = "playing"
+PAUSED = "paused"
+RIDDLE = "riddle"
+GAME_OVER = "game over"
+HOW_TO_PLAY = "how_to_play"
+ABOUT = "about"
+CONGRATS = "congrats"
+
+game_state = MENU
+
+
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("[RIDDLER]")
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+big_font = pygame.font.Font(None, 64)
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
