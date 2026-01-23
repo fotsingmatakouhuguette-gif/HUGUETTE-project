f9130cb2b62319192edb59e560c27d65306f7de7
eliz08ahanda-byte <eliz08.ahanda@gmail.com>
2026-01-23 04:35:42 +0100

    Update riddler.py
    added ASSETS, ENEMIES, UI ELEMENTS, SOUNDS

--- riddler.py
+++ riddler.py
@@ -34,6 +34,34 @@ clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 big_font = pygame.font.Font(None, 64)
 
+# ---------- ASSETS ----------
+sky=pygame.transform.scale(pygame.image.load("assets/backgrounds/sky.jpeg"), (WIDTH, HEIGHT))
+ground=pygame.transform.scale(pygame.image.load("assets/backgrounds/ground.jpeg"), (WIDTH, HEIGHT - GROUND_LEVEL))
+menu_list=pygame.transform.scale(pygame.image.load("assets/backgrounds/menu background.jpeg"), (WIDTH, HEIGHT))
+other_list=pygame.transform.scale(pygame.image.load("assets/backgrounds/listing.jpeg"), (WIDTH, HEIGHT))
+
+#---------- ENEMIES ----------
+enemy_img=pygame.image.load("assets/enemies/gary.jpeg").convert_alpha()
+flying_enemy_img=pygame.image.load("assets/enemies/flying.jpeg").convert_alpha()
+#---------- UI ELEMENTS ----------
+heart_img=pygame.tranform.scale(
+    pygame.image.load("assets/backgrounds/heart.jpeg").convert_alpha(), (32, 32)
+)
+congrats_message=pygame.transform.scale(
+    pygame.image.load("assets/buttons/congratulations.jpeg").convert_alpha(), (400, 100)
+)
+congrats_message_rect=congrats_message.get_rect(center=(400,200))
+game_over_message=pygame.transform.scale(
+    pygame.image.load("assets/buttons/game over.jpeg").convert_alpha(), (250, 100)
+)
+game_over_message_rect=game_over_message.get_rect(center=(400,200))
+#---------- SOUNDS ----------
+jump_sound=pygame.mixer.Sound("assets/songs/jump.wav")
+hit_sound=pygame.mixer.Sound("assets/songs/hit.wav")
+correct_sound=pygame.mixer.Sound("assets/songs/correct.wav")
+pygame.mixer.music.load("assets/songs/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
